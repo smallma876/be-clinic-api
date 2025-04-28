f81a8296acae1921a0119766dc2171eaaae97eba
luis <luis.mch154@gmail.com>
2025-04-28 01:02:50 -0500

    Create UserRepository for CRUD operations

---
+++ src/main/java/com/hopeclinic/api/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hopeclinic.api.repositories;
+
+import com.hopeclinic.api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
